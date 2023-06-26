abaca988c571de1b34ba01431a6756247c36cbb1
Brian Patino <Patinobrian@gmail.com>
2023-06-26 12:42:54 -0400

    add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Badge Buddy API')
+    .setDescription('The Badge Buddy API description')
+    .setVersion('1.0')
+    .addTag('badge-buddy')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
