8b27f88ad7aca1efd6608d72189ae13eae015c80
Brian <patinobrian@gmail.com>
2023-07-01 14:51:21 -0400

    wip

--- src/PinoLoggerService.ts
+++ src/PinoLoggerService.ts
@@ -3,14 +3,28 @@ import pino, { LoggerOptions } from 'pino';
 import ecsFormat from '@elastic/ecs-pino-format';
 
 export class PinoLoggerService implements LoggerService {
-  static readonly loggerOptions: LoggerOptions = {
-    // formats formatters, messageKey, timestamp options according to ECS
-    // https://www.elastic.co/guide/en/ecs-logging/nodejs/current/pino.html
-    ...ecsFormat({
-      apmIntegration: true,
-    }),
-    name: 'badge-buddy-api',
-  };
+  // static readonly loggerOptions: LoggerOptions = {
+  //   // formats formatters, messageKey, timestamp options according to ECS
+  //   // https://www.elastic.co/guide/en/ecs-logging/nodejs/current/pino.html
+  //   ...ecsFormat({
+  //     apmIntegration: true,
+  //   }),
+  //   name: 'badge-buddy-api',
+  //   transport: {
+  //     targets: [
+  //       {
+  //         level: 'info',
+  //         format: 'json',
+  //         target: 'poop',
+  //         options: {
+  //           destination: 'logs/app.log',
+  //           sync: false,
+  //           mkdir: true,
+  //         },
+  //       },
+  //     ],
+  //   },
+  // };
 
   constructor(private readonly pinoLogger: any) {}
 
@@ -19,13 +33,32 @@ export class PinoLoggerService implements LoggerService {
   }
 
   static createPino() {
-    return pino(
-      this.loggerOptions,
-      pino.destination({
-        dest: 'logs/app.log',
-        sync: false,
-      }),
-    );
+    const fileTransport = pino.transport({
+      targets: [
+        {
+          target: 'pino/file',
+          level: 'info',
+          options: {
+            destination: 'logs/app.log',
+            sync: false,
+            mkdir: true,
+          },
+        },
+        // {
+        //   target: 'pino-pretty',
+        //   level: 'info',
+        //   options: {
+        //     destination: '/dev/null',
+        //     sync: true,
+        //   },
+        // },
+      ],
+    });
+    return pino(fileTransport);
+    // name: 'badge-buddy-api',
+    // ...ecsFormat({
+    //   apmIntegration: true,
+    // }),
   }
 
   log(message: string) {
