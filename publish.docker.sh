5b716e8920adb7677a7b7c2687a57fb04e3a185b
SlinkyPotato <patinobrian@gmail.com>
2023-07-10 17:07:39 -0400

    abort push
    ignore husky folders
    
    add some sh logic
    
    check for ref branch
    
    adjust
    
    adjust
    
    adjust
    
    adjust
    
    test
    
    test
    
    add git params
    
    convert publish.docker.sh -> publish.docker.js

--- publish.docker.sh
+++
@@ -1,9 +0,0 @@
-NODE_ENV=$1;
-DOCKER_VERSION=$2;
-NODE_VERSION=$(cat package.json | jq -r ".engines.node");
-PNPM_VERSION=$(cat package.json | jq -r ".engines.pnpm");
-PROJECT_VERSION=$(cat package.json | jq -r ".version");
-
-docker image build --build-arg NODE_VERSION="${NODE_VERSION}" --build-arg PNPM_VERSION="${PNPM_VERSION}" --build-arg NODE_ENV="${NODE_ENV}" -t amaredeus/badge-buddy-api:"${DOCKER_VERSION}" .
-docker tag amaredeus/badge-buddy-api:"${DOCKER_VERSION}" amaredeus/badge-buddy-api:"${PROJECT_VERSION}"
-docker push -a amaredeus/badge-buddy-api
